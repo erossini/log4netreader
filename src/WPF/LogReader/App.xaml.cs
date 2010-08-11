91ef5ffcff9776ce5925259d9a62c3cc96526f22
masquerade2ruslan <masquerade2ruslan@a390c00f-ed18-9e62-aa84-9f156db45c12>
2010-08-11 20:39:00 +0000

    - created wpf start infrastructure

---
+++ src/WPF/LogReader/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.PresentationFramework.ApplicationModel;
+using LogReader.ViewModels;
+
+namespace LogReader
+{
+    public partial class App : CaliburnApplication
+    {
+        protected override object CreateRootModel()
+        {
+            return new ShellViewModel();
+        }
+    }
+}
---
+++ src/WPF/LogReader/Views/ShellView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LogReader.Views
+{
+    public partial class ShellView : UserControl
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
