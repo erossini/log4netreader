5c88d4eb4d9bc4c7ecbe18ed2b59f89a56b2d2a4
masquerade2ruslan <masquerade2ruslan@a390c00f-ed18-9e62-aa84-9f156db45c12>
2010-09-24 20:57:07 +0000

    - finished instant listeners

--- src/WPF/LogReader/Models/States/Paginators/FilePaginator.cs
+++ src/WPF/LogReader/Models/States/Paginators/FilePaginator.cs
@@ -1,44 +1,14 @@
-using System.Collections.ObjectModel;
-
 namespace LogReader.Models.States.Paginators
 {
-    public class FilePaginator : IPaginator
+    public class FilePaginator : Paginator
     {
-        private readonly IViewModel _viewModel;
-        private readonly Acumulator _acumulator;
-
-        public FilePaginator(IViewModel viewModel, Acumulator acumulator)
+        public FilePaginator(IViewModel viewModel, Acumulator acumulator) : base(viewModel, acumulator)
         {
-            _viewModel = viewModel;
-            _acumulator = acumulator;
         }
 
-        public void PreviousPage()
+        protected override string ContentMode()
         {
-            _acumulator.PreviosPage();
-            Bind();
-        }
-
-        public void NextPage()
-        {
-            _acumulator.NextPage();
-            Bind();
-        }
-
-        public void GoToPage(int index)
-        {
-            _acumulator.GoToPage(index);
-            Bind();
-        }
-
-        public void Bind()
-        {
-            _viewModel.Entries = new ObservableCollection<LogEntry>(_acumulator.GetActivePage());
-            _viewModel.Info = new PageInfo()
-            {
-                Pages = _acumulator.Pages,
-                PageIndex = _acumulator.CurrentPage
-            };
+            return "file content ...";
         }
     }
 }
--- src/WPF/LogReader/Models/States/Paginators/StreamPaginator.cs
+++ src/WPF/LogReader/Models/States/Paginators/StreamPaginator.cs
@@ -1,45 +1,36 @@
-using System.Collections.ObjectModel;
-using LogReader.Models.Observers;
-
 namespace LogReader.Models.States.Paginators
 {
-    public class StreamPaginator : IPaginator, IObserver
+    public class StreamPaginator : Paginator
     {
-        private readonly IViewModel _viewModel;
-        private readonly Acumulator _acumulator;
-
-        public StreamPaginator(IViewModel viewModel, Acumulator acumulator)
-        {
-            _viewModel = viewModel;
-            _acumulator = acumulator;
-
-            _acumulator.Subscribe(this);
-        }
-
-        public void PreviousPage()
+        public StreamPaginator(IViewModel viewModel, Acumulator acumulator) : base(viewModel, acumulator)
         {
         }
 
-        public void NextPage()
+        protected override string ContentMode()
         {
+            return "streaming ...";
         }
 
-        public void GoToPage(int index)
+        public override void Notify(object o)
         {
-        }
-
-        public void Bind()
-        {
-            _viewModel.Entries = new ObservableCollection<LogEntry>();
-        }
-
-        public void Notify(object o)
-        {
-            if (_acumulator.PageSize - 1 < _viewModel.Entries.Count)
-                _viewModel.Entries.RemoveAt(0);
+            if (Acumulator.PageSize - 1 < ViewModel.Entries.Count)
+                ViewModel.Entries.RemoveAt(0);
             
-            _viewModel.Entries.Add((LogEntry)o);
-            _viewModel.Info = new PageInfo() { Pages = _acumulator.Pages };
+            ViewModel.Entries.Add((LogEntry)o);
+
+            Acumulator.CurrentPage = Acumulator.Pages;
+
+            ShowPageInfo();
+        }
+
+        private void ShowPageInfo()
+        {
+            ViewModel.Info = new PageInfo()
+            {
+                Pages = Acumulator.Pages,
+                PageIndex = Acumulator.CurrentPage,
+                Mode = "streaming ..."
+            };
         }
     }
 }
